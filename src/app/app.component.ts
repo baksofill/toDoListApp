f790f666babb06fa5c41c88b0da6baa3d82f218c
Anton Obertynskyi [surface] <baksofill@mail.ru>
2020-03-01 21:31:22 +0200

    Some new changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { ItemsDataService } from './items-data.service';
+
+export interface Item {
+  title: string;
+  text: string;
+  idDone: boolean;
+  id?: number;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'my-todo-list';
+  items: Item[];
+
+  constructor(private itemsDataService: ItemsDataService) {
+    this.items = this.itemsDataService.getItems();
+    console.log(this.items);
+  }
+
+  updateItems(item: Item) {
+    this.itemsDataService.addNewItem(item);
+    // this.items.unshift(item);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListComponent } from './list/list.component';
+import { EditComponent } from './edit/edit.component';
+
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    EditComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
